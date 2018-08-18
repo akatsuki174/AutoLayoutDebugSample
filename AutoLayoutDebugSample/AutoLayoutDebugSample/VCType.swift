387acada6e569974ffd6f85af00127652280b4b5
akatsuki174 <dev2015js@gmail.com>
2018-08-18 11:54:55 +0900

    最初にどのVCに遷移するかを選択する画面を用意した

---
+++ AutoLayoutDebugSample/AutoLayoutDebugSample/VCType.swift
@@ -0,0 +1,8 @@
+enum VCType: String, EnumEnumerable {
+    case equalWidthView
+    case stackView
+    case coverView
+    case multiHierarchyView
+    case priorityView
+    case layoutByCodeView
+}
---
+++ AutoLayoutDebugSample/AutoLayoutDebugSample/ViewController/TopTableViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class TopTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension TopTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return VCType.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = VCType.cases[indexPath.row].rawValue
+        return cell
+    }
+}
