8776b1edf862436417ae703a6e200ca01ff65621
akatsuki174 <dev2015js@gmail.com>
2018-08-22 17:07:02 +0900

    制約出力をgreen view用とbase stack view用に分けた

--- AutoLayoutDebugSample/AutoLayoutDebugSample/ViewController/StackViewLayoutViewController.swift
+++ AutoLayoutDebugSample/AutoLayoutDebugSample/ViewController/StackViewLayoutViewController.swift
@@ -3,9 +3,12 @@ import UIKit
 final class StackViewLayoutViewController: UIViewController {
     @IBOutlet weak var baseStackView: UIStackView!
     @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var outputGreenViewConstraintsButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        outputGreenViewConstraintsButton.titleLabel?.numberOfLines = 0
+        outputGreenViewConstraintsButton.titleLabel?.lineBreakMode = .byWordWrapping
     }
 
     override func didReceiveMemoryWarning() {
@@ -13,9 +16,11 @@ final class StackViewLayoutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func tappedButton(_ sender: Any) {
-//        print(greenView.constraintsAffectingLayout(for: .vertical))
+    @IBAction func outputBaseStackViewConstraint(_ sender: Any) {
         self.view.constraintsReferringView(view: baseStackView)
     }
     
+    @IBAction func outputGreenViewConstraints(_ sender: Any) {
+        print(greenView.constraintsAffectingLayout(for: .vertical))
+    }
 }
